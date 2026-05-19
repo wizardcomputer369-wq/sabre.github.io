ad073275abc2af4f19509b872c9599718b9a31ee
wizardcomputer369-wq <wizardcomputer369@gmail.com>
2026-05-19 13:37:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+
 function scrollToContact() {
   document.getElementById("contact").scrollIntoView({
     behavior: "smooth"
   });
 }
+
+// Cookie banner
+function acceptCookies() {
+  localStorage.setItem("cookiesAccepted", "true");
+  document.getElementById("cookie-banner").style.display = "none";
+}
+
+window.onload = function() {
+  if (localStorage.getItem("cookiesAccepted") === "true") {
+    document.getElementById("cookie-banner").style.display = "none";
+  }
+};
